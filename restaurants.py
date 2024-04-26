07eba5ff830b77e669e32f18d5119ccde40f2d44
SFrasia <108404961+SFrasia@users.noreply.github.com>
2024-04-26 04:37:12 +0300

    Aggragate

--- restaurants.py
+++ restaurants.py
@@ -1,11 +1,12 @@
-#Initializers and Properties
 class Customer:
-
-    all= []
 
     def __init__(self, first_name, last_name):
         self.first_name = first_name
         self.last_name = last_name
+        self.first_name = None
+        self.last_name = None
+        self.reviews = []
+        
 
     @property
     def first_name(self):
@@ -31,13 +32,17 @@ class Customer:
              raise TypeError('Names must be between 1 and 25 characters')
          self.second_name = value
 
+    def num_negative_reviews(self):
+        return sum(1 for review in self.reviews if review.rating in [1, 2])
+    def has_reviewed_restaurant(self, restaurant):
+        return (reviews.restaurant == restaurant for reviews in self.reviews)
+
 
 class Restaurant:
 
-    all = []
-
     def __init__(self, name):
       self.name = name
+      self.reviews = []
 
     @property
     def restaurant(self):
@@ -51,9 +56,21 @@ class Restaurant:
             raise TypeError("Names must be 1 or more characters")
         self._restaurant = value
 
-class Reviews:
+    def average_start_rating(self):
+        if not self.reviews:
+            return 0.0
+        total_ratings = sum(review.rating for review in self.reviews)
+        average_rating = total_ratings / len(self.reviews)
+        return round(average_rating, 1)
     
-    all = []
+    @classmethod
+    def top_two_restaurants(cls):
+        if not cls.members:
+            return None
+        sorted_restaurants = sorted(cls.members, key = lambda restaurant: restaurant.average_star_rating(), reverse = True)
+        return sorted_restaurants[:2]
+
+class Reviews:
 
     def __init__(self, customer, restaurant, rating):
         self.customer = customer
@@ -70,5 +87,3 @@ class Reviews:
             raise TypeError('Ratings must be of type integer')
         if not 1 <= len(value) <= 5:
             raise TypeError("Ratings must be between 1 and 5")
-        
-# Object Relationship Methods and Properties
