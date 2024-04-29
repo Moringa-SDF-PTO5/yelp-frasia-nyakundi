170c5374d104b9d8ab2c39850e23fa553adb8c09
SFrasia <108404961+SFrasia@users.noreply.github.com>
2024-04-29 08:52:14 +0300

    changes

--- restaurants.py
+++ restaurants.py
@@ -1,13 +1,12 @@
 class Customer:
 
     def __init__(self, first_name, last_name):
-        self.first_name = first_name
-        self.last_name = last_name
         self.first_name = None
         self.last_name = None
+        self.first_name = first_name
+        self.last_name = last_name
         self.reviews = []
         
-
     @property
     def first_name(self):
          return self._first_name
@@ -15,25 +14,37 @@ class Customer:
     @first_name.setter
     def first_name(self, value):
          if not isinstance(value, str):
-              raise TypeError('Names must be of type str')
-         if not 1<= len(value) <= 25:
+              raise TypeError('Names must be of type string')
+         if not 1 <= len(value) <= 25:
              raise TypeError('Names must be between 1 and 25 characters')
          self.first_name = value
          
     @property
-    def second_name(self):
-         return self._second_name
+    def last_name(self):
+         return self._last_name
     
-    @second_name.setter
-    def second_name(self, value):
+    @last_name.setter
+    def last_name(self, value):
          if not isinstance(value, str):
               raise TypeError('Names must be of type string')
          if not 1 <= len(value) <= 25:
-             raise TypeError('Names must be between 1 and 25 characters')
-         self.second_name = value
+             raise ValueError('Names must be between 1 and 25 characters')
+         self.last_name = value
+
+    def add_review(self, review):
+        if not isinstance(review, Reviews):
+            raise TypeError("Reviews must be of type Review")
+        self._add_reviews.append(review)
+
+    def reviews(self):
+        return self._reviews
+
+    def restaurants(self):
+        return list({review.restaurant for review in self.reviews})
 
     def num_negative_reviews(self):
         return sum(1 for review in self.reviews if review.rating in [1, 2])
+    
     def has_reviewed_restaurant(self, restaurant):
         return (reviews.restaurant == restaurant for reviews in self.reviews)
 
@@ -41,20 +52,32 @@ class Customer:
 class Restaurant:
 
     def __init__(self, name):
+      self.name = None
       self.name = name
       self.reviews = []
 
     @property
-    def restaurant(self):
-        return self._restaurant
+    def name(self):
+        return self._name
     
-    @restaurant.setter
+    @name.setter
     def restaurant(self, value):
         if not isinstance(value, str):
             raise TypeError('Names must be of type string')
         if not 1 <= len(value) <= 100:
-            raise TypeError("Names must be 1 or more characters")
+            raise ValueError("Names must be 1 or more characters")
         self._restaurant = value
+
+    def add_review(self, review):
+        if not isinstance(review, Reviews):
+            raise TypeError("Reviews must be of type Review")
+        self._reviews.append(review)
+
+    def reviews(self):
+        return self._reviews
+    
+    def customers(self):
+        return list({review.customer for review in self.reviews})
 
     def average_start_rating(self):
         if not self.reviews:
@@ -70,20 +93,45 @@ class Restaurant:
         sorted_restaurants = sorted(cls.members, key = lambda restaurant: restaurant.average_star_rating(), reverse = True)
         return sorted_restaurants[:2]
 
+
 class Reviews:
 
     def __init__(self, customer, restaurant, rating):
+        self.customer = None
+        self.restaurant = None
         self.customer = customer
         self.restaurant = restaurant
         self.rating = rating
 
     @property
     def rating(self):
-        self._rating
+        return self._rating
 
     @rating.setter
     def rating (self, value):
         if not isinstance(value, int):
             raise TypeError('Ratings must be of type integer')
-        if not 1 <= len(value) <= 5:
-            raise TypeError("Ratings must be between 1 and 5")
+        if not 1 <= value <= 5:
+            raise ValueError("Ratings must be between 1 and 5")
+        self._rating = value
+
+    @property
+    def customer(self):
+        return self._customer
+    
+    @customer.setter
+    def customer(self, value):
+        if not isinstance(value, Customer):
+            raise TypeError("Customer must be type of Customer")
+        self._customer = value
+
+    @property
+    def restaurant(self):
+        return self._restaurant
+    
+    @restaurant.setter
+    def restaurant (self, value):
+        if not isinstance(value, Restaurant):
+            raise TypeError("Restaurant must be type of Restaurant")
+        self._restaurant = value
+
